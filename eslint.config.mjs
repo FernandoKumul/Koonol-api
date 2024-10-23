081338d10f2af1ea570db11e156e3bedb7be0d7b
raulmay8 <raulmay8@hotmail.com>
2024-10-23 17:40:59 -0500

    configuración de husky, eslint y pipeline

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+//eslint.config.mjs
+import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "no-unused-vars": "error",  // Mostrar error para variables no usadas
+    }
+  }
+];
