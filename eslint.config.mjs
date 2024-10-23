7a142cfd453fa8073d7dd5e86a923f6ed6ce7360
raulmay8 <raulmay8@hotmail.com>
2024-10-23 18:20:28 -0500

    mejora de eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,45 @@
-//eslint.config.mjs
 import globals from "globals";
-import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import path from "path";
 
 export default [
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      globals: globals.browser,
-      parser: "@typescript-eslint/parser",
-      ecmaVersion: 2020,
-      sourceType: "module",
+      parser: parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        project: "./tsconfig.json",
+        tsconfigRootDir: path.resolve(__dirname),
+      },
+      globals: {
+        ...globals.browser,
+        // Añade otros globals si es necesario
+      },
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      "@typescript-eslint": tsPlugin,
     },
     rules: {
-      ...tseslint.configs.recommended.rules,
-      "no-unused-vars": "error",  // Mostrar error para variables no usadas
-    }
-  }
+      ...tsPlugin.configs.recommended.rules,
+      "no-unused-vars": "error", // Mostrar error para variables no usadas
+      // Añade o sobrescribe otras reglas si es necesario
+    },
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        // Añade otros globals si es necesario
+      },
+    },
+    rules: {
+      // Reglas para archivos JavaScript
+    },
+  },
 ];
