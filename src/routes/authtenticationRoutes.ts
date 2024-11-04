cfb43a3881d9b88ca934e8e8531ef73088bcbeda
Luis Flores <luisgomez7829@gmail.com>
2024-11-04 18:41:47 -0500

    agregar validación de token y ruta correspondiente en el controlador de autenticación

--- src/routes/authtenticationRoutes.ts
+++ src/routes/authtenticationRoutes.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import AuthController from '../controllers/authenticactionController';
 import { validateData } from '../middleware/validationMiddleware';
 import { loginSchema } from '../schemas/AuthenticationSchema/authenticacionSchema';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.post('/login', validateData(loginSchema), AuthController.login);
+router.get('/validate',authMiddleware, AuthController.validateToken);
 
 export default router;
